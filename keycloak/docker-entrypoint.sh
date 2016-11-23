0585daafe665ae42bc496b4d5fd8ec5d61fb3ffb
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2016-11-23 09:58:34 -0800

    Set redirect uris at runtime from envvar

--- keycloak/docker-entrypoint.sh
+++ keycloak/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -z ${REDIRECT_URIS+x} ]; then
+  echo 'REDIRECT_URIS environment variable not set' >&2
+  exit 1
+else
+  sed -i "s@{{REDIRECT_URIS}}@$REDIRECT_URIS@g" import/hmda-realm.json
+  echo "Keycloak redirect uris set to $REDIRECT_URIS"
+fi
+
 if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
     keycloak/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
 fi
@@ -9,10 +17,9 @@ if [ $INSTITUTION_SEARCH_URI ]; then
     echo 'Keycloak "login" theme.properties updated:'
     cat keycloak/themes/hmda/login/theme.properties
 else
-    echo 'INSTITUTION_SEARCH_URI environmental variable not set' >&2
+    echo 'INSTITUTION_SEARCH_URI environment variable not set' >&2
     exit 1
 fi
-
 
 exec /opt/jboss/keycloak/bin/standalone.sh $@
 exit $?
